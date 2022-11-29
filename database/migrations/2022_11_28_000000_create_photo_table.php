b0496c8af7085c18af81c48e2f845c9b36590f2b
Вениамин Смородинский <origamiv@yandex.ru>
2022-11-29 13:16:57 +0300

    migrations

--- database/migrations/2022_11_28_000000_create_photo_table.php
+++ database/migrations/2022_11_28_000000_create_photo_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('photo', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('photo_id');
+            $table->integer('status')->default(0)->comment('0-new,1-accepted,2-declined');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('photo');
     }
 };
